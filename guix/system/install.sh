02908873561a264db466b43c173849ed5d06555b
Jáchym Tomášek <jachym.tomasek@tutanota.com>
2022-03-18 10:54:26 +0100

    add installation script

---
+++ guix/system/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [[$1="casper"]]; then
+     mkfs.fat -F32 -n boot $2
+     mkfs.btrfs -L system $3
+else
+    echo "unrecognized option, please modify this script"
+    exit
+fi
+
+mount LABEL=system /mnt
+mkdir -p /mnt/boot/efi
+mount LABEL=boot /mnt/boot/efi
+
+herd start cow-store /mnt
+
+mkdir -p ~/.config/guix
+cp ./magi/guix/channels.scm ~/.config/guix
+guix pull
+hash guix
+
+guix system -L ~/magi/guix/system init ~/magi/guix/system/"$1".scm /mnt
